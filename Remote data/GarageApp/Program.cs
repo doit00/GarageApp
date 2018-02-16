b50735ebae0c51cdd754bde5b7b3998bdaf4e27a
doit00 <36489572+doit00@users.noreply.github.com>
2018-02-16 09:13:45 +0100

    WebClient, WebRequest

--- Remote data/GarageApp/Program.cs
+++ Remote data/GarageApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,28 @@ namespace GarageApp
     {
         static void Main(string[] args)
         {
+            WebClientHttp();
+            WebRequestHtp();
+
+        }
+
+        private static void WebRequestHtp()
+        {
+            var req = WebRequest.Create("http://google.pl") as HttpWebRequest;
+            req.Method = "GET";
+            var resp = req.GetResponse() as HttpWebResponse;
+            Console.WriteLine(resp.StatusCode);
+            var rs = resp.GetResponseStream();
+            rs.CopyTo(new FileStream("google.html", FileMode.CreateNew));
+        }
+
+        private static void WebClientHttp()
+        {
+            WebClient cli = new WebClient();
+            var page = cli.DownloadString("http://google.pl");
+            File.AppendAllText("out.html", page);
+            cli.Dispose();
         }
     }
+
 }
